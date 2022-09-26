26149f72297ab3786fa974068ca26fa4e1ff8e5a
khoathu <khoathu89@gmail.com>
2022-09-27 00:35:29 +0700

    Architech Skeleton

---
+++ app/src/main/java/com/example/newsappmvvm/repository/NewsRepository.kt
@@ -0,0 +1,9 @@
+package com.example.newsappmvvm.repository
+
+import com.example.newsappmvvm.db.ArticleDatabase
+
+class NewsRepository(
+    val db: ArticleDatabase
+) {
+
+}
--- app/src/main/java/com/example/newsappmvvm/ui/NewsActivity.kt
+++ app/src/main/java/com/example/newsappmvvm/ui/NewsActivity.kt
@@ -3,16 +3,33 @@ package com.example.newsappmvvm.ui
 import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.newsappmvvm.R
+import com.example.newsappmvvm.db.ArticleDatabase
+import com.example.newsappmvvm.repository.NewsRepository
+import com.example.newsappmvvm.viewmodels.NewsViewModel
 import kotlinx.android.synthetic.main.activity_news.*
 
 class NewsActivity: AppCompatActivity() {
+
+    lateinit var viewModel: NewsViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_news)
 
-        bottomNavigationView.setupWithNavController(newsNavHostFragment.findNavController())
+        val newsRepository = NewsRepository(ArticleDatabase(this))
+        val viewModelProviderFactory = NewsViewModelProviderFactory(newsRepository)
+        viewModel = ViewModelProvider(this, viewModelProviderFactory).get(NewsViewModel::class.java)
+
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.newsNavHostFragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        bottomNavigationView.setupWithNavController(navController)
     }
 }
