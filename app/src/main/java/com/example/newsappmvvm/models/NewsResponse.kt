37b99fb4aaee6afa507ac0800d7bc92d5b3adf23
khoathu <37234414+khoathu@users.noreply.github.com>
2022-09-27 14:39:52 +0700

    Revert "Handle breaking news response"

--- app/src/main/java/com/example/newsappmvvm/models/NewsResponse.kt
+++
@@ -1,7 +0,0 @@
-package com.example.newsappmvvm.models
-
-data class NewsResponse(
-    val articles: List<Article>,
-    val status: String,
-    val totalResults: Int
-)
--- app/src/main/java/com/example/newsappmvvm/models/Source.kt
+++
@@ -1,6 +0,0 @@
-package com.example.newsappmvvm.models
-
-data class Source(
-    val id: Any,
-    val name: String
-)
