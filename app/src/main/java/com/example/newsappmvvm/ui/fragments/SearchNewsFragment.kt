79fc26830d190761d653d325aafd5a676a0d2702
khoathu <khoathu89@gmail.com>
2022-09-27 16:47:23 +0700

    Handle search news

--- app/src/main/java/com/example/newsappmvvm/ui/fragments/SearchNewsFragment.kt
+++ app/src/main/java/com/example/newsappmvvm/ui/fragments/SearchNewsFragment.kt
@@ -1,17 +1,82 @@
 package com.example.newsappmvvm.ui.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.newsappmvvm.R
+import com.example.newsappmvvm.adapters.NewsAdapter
 import com.example.newsappmvvm.ui.NewsActivity
+import com.example.newsappmvvm.utils.Constants.Companion.SEARCH_NEWS_TIME_DELAY
+import com.example.newsappmvvm.utils.Resource
 import com.example.newsappmvvm.viewmodels.NewsViewModel
+import kotlinx.android.synthetic.main.fragment_search_news.*
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class SearchNewsFragment : Fragment(R.layout.fragment_search_news) {
-    lateinit var viewModel : NewsViewModel
+    lateinit var viewModel: NewsViewModel
+    lateinit var newsAdapter: NewsAdapter
+
+    val TAG = "SearchNewsFragment"
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel = (activity as NewsActivity).viewModel
+        setupRecycleView()
+
+        var job: Job? = null
+        edSearch.addTextChangedListener { editable ->
+            job?.cancel()
+            job = MainScope().launch {
+                delay(SEARCH_NEWS_TIME_DELAY)
+                editable?.let {
+                    if (it.toString().isNotEmpty()) {
+                        viewModel.searchNews(it.toString())
+                    }
+                }
+            }
+        }
+
+        viewModel.searchNews.observe(viewLifecycleOwner, Observer { response ->
+            when (response) {
+                is Resource.Success -> {
+                    hideProgressBar()
+                    response.data?.articles?.let {
+                        newsAdapter.differ.submitList(it)
+                    }
+                }
+                is Resource.Error -> {
+                    hideProgressBar()
+                    response.message?.let {
+                        Log.e(TAG, "Error is occurred: $it")
+                    }
+                }
+                is Resource.Loading -> {
+                    showProgressBar()
+                }
+            }
+        })
+    }
+
+    private fun setupRecycleView() {
+        newsAdapter = NewsAdapter()
+        rvSearchNews.apply {
+            adapter = newsAdapter
+            layoutManager = LinearLayoutManager(activity)
+        }
+    }
+
+    fun showProgressBar() {
+        paginationProgressBar.visibility = View.VISIBLE
+    }
+
+    fun hideProgressBar() {
+        paginationProgressBar.visibility = View.INVISIBLE
     }
 }
--- app/src/main/java/com/example/newsappmvvm/viewmodels/NewsViewModel.kt
+++ app/src/main/java/com/example/newsappmvvm/viewmodels/NewsViewModel.kt
@@ -13,8 +13,11 @@ class NewsViewModel(
     val newsRepository: NewsRepository
 ) : ViewModel() {
 
-    val breakingNews : MutableLiveData<Resource<NewsResponse>> = MutableLiveData()
+    val breakingNews: MutableLiveData<Resource<NewsResponse>> = MutableLiveData()
     var breakingNewsPage = 1
+
+    var searchNews: MutableLiveData<Resource<NewsResponse>> = MutableLiveData()
+    var searchNewsPage = 1
 
     init {
         getBreakingNews("us")
@@ -25,8 +28,24 @@ class NewsViewModel(
         val response = newsRepository.getBreakingNews(countryCode, breakingNewsPage)
         breakingNews.postValue(handleBreakingNewsResponse(response))
     }
-    private fun handleBreakingNewsResponse(response: Response<NewsResponse>) : Resource<NewsResponse> {
-        if(response.isSuccessful) {
+
+    fun searchNews(query: String) = viewModelScope.launch {
+        searchNews.postValue(Resource.Loading())
+        val response = newsRepository.searchNews(query, searchNewsPage)
+        searchNews.postValue(handleSearchNewsResponse(response))
+    }
+
+    private fun handleSearchNewsResponse(response: Response<NewsResponse>): Resource<NewsResponse> {
+        if (response.isSuccessful) {
+            response.body()?.let { resultResponse ->
+                return Resource.Success(resultResponse)
+            }
+        }
+        return Resource.Error(response.message())
+    }
+
+    private fun handleBreakingNewsResponse(response: Response<NewsResponse>): Resource<NewsResponse> {
+        if (response.isSuccessful) {
             response.body()?.let { resultResponse ->
                 return Resource.Success(resultResponse)
             }
