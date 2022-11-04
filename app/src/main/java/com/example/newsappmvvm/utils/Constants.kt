4892c33e9383b9cd8c54d611b0f2d7fd0b945f5e
khoathu <khoathu89@gmail.com>
2022-11-04 12:10:35 +0700

    implements : hide API_KEY in local.properties

--- app/src/main/java/com/example/newsappmvvm/utils/Constants.kt
+++ app/src/main/java/com/example/newsappmvvm/utils/Constants.kt
@@ -1,9 +1,11 @@
 package com.example.newsappmvvm.utils
 
+import com.example.newsappmvvm.BuildConfig
+
 class Constants {
     companion object
     {
-        const val API_KEY = "6953a7421bb749c589e687746c85eee8"
+        const val API_KEY = BuildConfig.API_KEY
         const val BASE_URL = "https://newsapi.org"
         const val SEARCH_NEWS_TIME_DELAY = 700L
         const val QUERY_PAGE_SIZE = 20
